a3d6ca3afea14a1ad36038e5adcb92764a2dc6e8
ericmiguel <eric.mrib@gmail.com>
2022-11-04 11:09:35 -0300

    :scroll: reference pages

---
+++ docs/converters/numpy.md
@@ -0,0 +1,3 @@
+# Numpy converter
+
+Under construction.
--- docs_src/datetime/datetime_intro_1.py
+++ docs_src/datetime/datetime_intro_1.py
@@ -4,4 +4,4 @@ import timeless
 dt = timeless.datetime(1994, 2, 26)
 
 dt.zone  # => 'UTC'
-dt.zone_info  # => zoneinfo.ZoneInfo(key='UTC')
+dt.tzinfo  # => zoneinfo.ZoneInfo(key='UTC')
